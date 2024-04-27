943144c7127a41d2acd980d351124a17bb0c20be
golive6 <svravikiran.goli@gmail.com>
2024-04-26 21:53:51 -0400

    shell script practice

--- backend-installation.sh
+++ backend-installation.sh
@@ -1,1 +1,100 @@
 #!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "Please enter DB Password:"
+read mysql_root_password
+
+echo "scripting started executing at: $TIMESTAMP"
+
+VALIDATE(){     #function is checking if the installation is SUCCESS or FAILURE.
+if [ $1 -ne 0 ]
+then 
+    echo -e "$2....$R FAILURE $N"
+    exit 1
+else
+    echo -e "$2....$G SUCCESS $N"
+fi
+
+}
+
+if [ $USERID -ne 0 ] #checking if the USERID is 0 or not, if 0 it is a SUPER-USER, will have access for installation and exit status is 0
+then
+    echo "please run this script with root access."
+    exit 1 #manually exit if there is a error.
+else
+    echo "You are a Super User"
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs version 20"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
+
+id expense &>>LOGFILE
+if [ $? -ne 0 ]
+then 
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "Expense user is already created..... $Y SKIPPING $N"
+if
+
+mkdir -p /app
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading backend code"
+
+cd /app
+rm -rf /app/*
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracting backend code"
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing nodejs dependencies"
+
+cp /home/ec2-user/expense-shell-new/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copied backend service file"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon reloading"
+
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Starting backend"
+
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling backend"
+
+
+mysql -h db.goliexpense.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
+systemctl restart backend
+VALIDATE $? "Restarting backend"
+
+
+
+
+
+
+
+
+
+
+
+
+
--- frontend-installation.sh
+++ frontend-installation.sh
@@ -1,1 +1,63 @@
 #!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "scripting started executing at: $TIMESTAMP"
+
+VALIDATE(){     #function is checking if the installation is SUCCESS or FAILURE.
+if [ $1 -ne 0 ]
+then 
+    echo -e "$2....$R FAILURE $N"
+    exit 1
+else
+    echo -e "$2....$G SUCCESS $N"
+fi
+
+}
+
+if [ $USERID -ne 0 ] #checking if the USERID is 0 or not, if 0 it is a SUPER-USER, will have access for installation and exit status is 0
+then
+    echo "please run this script with root access."
+    exit 1 #manually exit if there is a error.
+else
+    echo "You are a Super User"
+fi
+
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling Nginx"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "Starting Nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE "Removing existing content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE "Downloading frontend code"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE "Extracting frontend code"
+
+cp /home/ec2-user/expense-shell-new/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE "Copied expense conf file"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarting Nginx"
+
+
+
+
+
+
